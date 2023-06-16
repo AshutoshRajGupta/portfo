3b81a3ee2ddb463f4ffa8aa69fd4374528948edd
Ashutosh Raj Gupta <ag2364443@gmail.com>
2023-06-17 01:30:54 +0530

    updated projects

--- src/App.js
+++ src/App.js
@@ -20,6 +20,8 @@ import web3 from "./images/p3.png";
 import web4 from "./images/p4.png";
 import web5 from "./images/p5.png";
 import web6 from './images/p6.png';
+import advnc from './images/advnc1.png';
+import advnc1 from './images/advnc2.png';
 import resumePDF from './images/resume.pdf';
 
 
@@ -224,7 +226,7 @@ function App() {
           </div>
         </section>
 
-        <section className="py-10">
+        {/* <section className="py-10">
           <div>
             <h3 className="text-3xl py-1 font-bold dark:text-white">
               Projects
@@ -420,7 +422,321 @@ function App() {
               </div>
             </div>
           </div>
-        </section>
+        </section> */}
+
+        <section className="py-10">
+  <div>
+    <h3 className="text-3xl py-1 font-bold dark:text-white">
+      Projects
+    </h3>
+    <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+      Since the beginning of my journey as a freelance designer and
+      developer, I've done remote work for{" "}
+      <span className="text-teal-500">agencies</span>, consulted for{" "}
+      <span className="text-teal-500">startups</span>, and collaborated
+      with talented people to create digital products for both business
+      and consumer use.
+    </p>
+    <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+      I offer a wide range of services, including brand design,
+      programming, and teaching.
+    </p>
+  </div>
+  
+  {/* Beginner Level */}
+  <div className="py-10">
+    <h4 className="text-2xl py-2 font-bold dark:text-white text-center">
+      Beginner Level
+    </h4>
+    
+    <div className="flex flex-col gap-10 lg:flex-row lg:flex-wrap">
+
+    <div className="basis-1/3 flex-1">
+        <img
+          className="rounded-lg object-cover"
+          width={500}
+          height={250}
+          layout="responsive"
+          src={web2}
+          alt="Project 2"
+        />
+        <p className="text-lg py-2 font-medium text-blue-900 dark:text-blue-400">
+          Random Advice Generator
+        </p>
+  
+        <p className="text-red-600">
+          Random Advice Generator made using ReactJs with advice API.<br />
+          Users can click on the button to get random advice.
+        </p>
+        <div className="flex justify-center mt-4">
+          <a
+            href="https://github.com/AshutoshRajGupta/Random_quote_generator" target="_blank"
+            className="btn-primary mr-30 px-4 py-2 bg-teal-600 text-white border border-teal-600 hover:bg-teal-800 hover:border-teal-800"
+          >
+            Github
+          </a>
+          <a
+            href="https://random-quote-generator-woad.vercel.app/" target="_blank"
+            className="btn-secondary px-4 py-2 bg-gray-300 text-gray-800 border border-gray-300 hover:bg-gray-400 hover:border-gray-400"
+          >
+            Visit Website
+          </a>
+        </div>
+      </div>
+
+      <div className="basis-1/3 flex-1">
+        <img
+          className="rounded-lg object-cover"
+          width={500}
+          height={250}
+          layout="responsive"
+          src={web1}
+          alt="Project 1"
+        />
+        <p className="text-lg py-2 font-medium text-blue-900 dark:text-blue-400">
+          ToDo Application
+        </p>
+  
+        <p className="text-red-600">
+          ToDo Application made using ReactJs with localStorage.<br />
+          Users can edit, add, delete, and mark todos as complete.
+        </p>
+        <div className="flex justify-center mt-4">
+          <a
+            href="https://github.com/AshutoshRajGupta/Todo_react" target="_blank"
+            className="btn-primary mr-30 px-4 py-2 bg-teal-600 text-white border border-teal-600 hover:bg-teal-800 hover:border-teal-800"
+          >
+            Github
+          </a>
+          <a
+            href="https://todo-react-six-red.vercel.app/" target="_blank"
+            className="btn-secondary px-4 py-2 bg-gray-300 text-gray-800 border border-gray-300 hover:bg-gray-400 hover:border-gray-400"
+          >
+            Visit Website
+          </a>
+        </div>
+      </div>
+      
+      {/* Add more beginner level projects here */}
+  
+    </div>
+  </div>
+  
+  {/* Intermediate Level */}
+  <div className="py-10">
+    <h4 className="text-2xl py-2 font-bold dark:text-white text-center">
+      Intermediate Level
+    </h4>
+  
+    <div className="flex flex-col gap-10 lg:flex-row lg:flex-wrap">
+
+    <div className="basis-1/3 flex-1">
+        <img
+          className="rounded-lg object-cover"
+          width={500}
+          height={250}
+          layout="responsive"
+          src={web3}
+          alt="Project 3"
+        />
+        <p className="text-lg py-2 font-medium text-blue-900 dark:text-blue-400">
+          Ecommerce Website
+        </p>
+  
+        <p className="text-red-600">
+          Ecommerce website made using PHP, MySQL, and HTML.<br />
+          Users can login, sign in, add items to the cart, and proceed to payment.
+        </p>
+        <div className="flex justify-center mt-4">
+          <a
+            href="" target="_blank"
+            className="btn-primary mr-30 px-4 py-2 bg-teal-600 text-white border border-teal-600 hover:bg-teal-800 hover:border-teal-800"
+          >
+            Github
+          </a>
+          <a
+            href="" target="_blank"
+            className="btn-secondary px-4 py-2 bg-gray-300 text-gray-800 border border-gray-300 hover:bg-gray-400 hover:border-gray-400"
+          >
+            Visit Website
+          </a>
+        </div>
+      </div>
+      
+
+      <div className="basis-1/3 flex-1">
+              <img
+                className="rounded-lg object-cover"
+                width={500}
+                height={250}
+                layout="responsive"
+                src={web4}
+                alt="Project 4"
+              />
+              <p className="text-lg py-2 font-medium text-blue-900 dark:text-blue-400">
+                Event Management System
+              </p>
+
+              <p className="text-red-600">Event management system made for college studentto register and<br></br> aprticipate in event.User can register then login go payment fees etc. </p>
+              <div className="flex justify-center mt-4">
+                <a
+                  href="https://github.com/AshutoshRajGupta/WTL_MIni_Project" target="_blank"
+                  className="btn-primary mr-30 px-4 py-2 bg-teal-600 text-white border border-teal-600 hover:bg-teal-800 hover:border-teal-800"
+                >
+                  Github
+                </a>
+                <a
+                                    href="https://github.com/AshutoshRajGupta/WTL_MIni_Project" target="_blank"
+
+                  className="btn-secondary px-4 py-2 bg-gray-300 text-gray-800 border border-gray-300 hover:bg-gray-400 hover:border-gray-400"
+                >
+                  Visit Website
+                </a>
+              </div>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <img
+                className="rounded-lg object-cover"
+                width={500}
+                height={250}
+                layout="responsive"
+                src={web5}
+                alt="Project 5"
+              />
+              <p className="text-lg py-2 font-medium text-blue-900 dark:text-blue-400">
+                Map2procoder
+              </p>
+
+              <p className="text-red-600">A website which describes the learning path for different domains</p>
+              <div className="flex justify-center mt-4">
+                <a
+                  href="https://github.com/AshutoshRajGupta/map2proCoder" target="_blank"
+                  className="btn-primary mr-30 px-4 py-2 bg-teal-600 text-white border border-teal-600 hover:bg-teal-800 hover:border-teal-800"
+                >
+                  Github
+                </a>
+                <a
+                  href="https://map2procoder.vercel.app/" target="_blank"
+                  className="btn-secondary px-4 py-2 bg-gray-300 text-gray-800 border border-gray-300 hover:bg-gray-400 hover:border-gray-400"
+                >
+                  Visit Website
+                </a>
+              </div>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <img
+                className="rounded-lg object-cover"
+                width={500}
+                height={250}
+                layout="responsive"
+                src={web6}
+                alt="Project 6"
+              />
+              <p className="text-lg py-2 font-medium text-blue-900 dark:text-blue-400">
+                Expense Budget App
+              </p>
+
+              <p className="text-red-600">Budget app made using reactjs and vitejs to manage your budget and expense</p>
+              <div className="flex justify-center mt-4">
+                <a
+                  href="https://github.com/AshutoshRajGupta/expense_try" target="_blank"
+                  className="btn-primary mr-30 px-4 py-2 bg-teal-600 text-white border border-teal-600 hover:bg-teal-800 hover:border-teal-800"
+                >
+                  Github
+                </a>
+                <a
+                  href="https://expense-try.vercel.app" target="_blank"
+                  className="btn-secondary px-4 py-2 bg-gray-300 text-gray-800 border border-gray-300 hover:bg-gray-400 hover:border-gray-400"
+                >
+                  Visit Website
+                </a>
+              </div>
+            </div>
+
+      
+      
+      {/* Add more intermediate level projects here */}
+  
+    </div>
+  </div>
+  
+  {/* Advanced Level */}
+  <div className="py-10">
+    <h4 className="text-2xl py-2 font-bold dark:text-white text-center">
+      Advanced Level
+    </h4>
+  
+    <div className="flex flex-col gap-10 lg:flex-row lg:flex-wrap">
+      <div className="basis-1/3 flex-1">
+        <img
+          className="rounded-lg object-cover"
+          width={500}
+          height={250}
+          layout="responsive"
+          src={advnc}
+          alt="Project 3"
+        />
+        <p className="text-lg py-2 font-medium text-blue-900 dark:text-blue-400">
+          WorkOut Website
+        </p>
+  
+        <p className="text-red-600">
+          A mern stack workout website made using mongodb reactjs express nodejs<br></br> where user can add information about his workout and <br></br>he can delete the workout and all the workouts will be save on the database.
+        </p>
+        <div className="flex justify-center mt-4">
+          <a
+            href="" target="_blank"
+            className="btn-primary mr-30 px-4 py-2 bg-teal-600 text-white border border-teal-600 hover:bg-teal-800 hover:border-teal-800"
+          >
+            Github
+          </a>
+          <a
+            href="" target="_blank"
+            className="btn-secondary px-4 py-2 bg-gray-300 text-gray-800 border border-gray-300 hover:bg-gray-400 hover:border-gray-400"
+          >
+            Visit Website
+          </a>
+        </div>
+      </div>
+
+      <div className="basis-1/3 flex-1">
+        <img
+          className="rounded-lg object-cover"
+          width={500}
+          height={250}
+          layout="responsive"
+          src={advnc1}
+          alt="Project 3"
+        />
+        <p className="text-lg py-2 font-medium text-blue-900 dark:text-blue-400">
+          Crypto Website
+        </p>
+  
+        <p className="text-red-600">
+          A crypto currency website made using ReactJs and rapid API through which <br></br> users can easily acess the data of every crypto currency <br></br> and user can get the data through visiual representation.
+        </p>
+        <div className="flex justify-center mt-4">
+          <a
+            href="https://github.com/AshutoshRajGupta/cryptoworld" target="_blank"
+            className="btn-primary mr-30 px-4 py-2 bg-teal-600 text-white border border-teal-600 hover:bg-teal-800 hover:border-teal-800"
+          >
+            Github
+          </a>
+          <a
+            href="https://github.com/AshutoshRajGupta/cryptoworld" target="_blank"
+            className="btn-secondary px-4 py-2 bg-gray-300 text-gray-800 border border-gray-300 hover:bg-gray-400 hover:border-gray-400"
+          >
+            Visit Website
+          </a>
+        </div>
+      </div>
+      
+      {/* Add more advanced level projects here */}
+  
+    </div>
+  </div>
+</section>
+
 
         <section className="bg-blue-200 py-10">
           <div className="container mx-auto">
